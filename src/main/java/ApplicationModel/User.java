d90e95b8c37a957dadb40d26474f8da4ead846c9
kavin <pv3442ia@go.minnstate.edu>
2024-09-22 21:25:26 -0500

    Added main class to scr/main/java

---
+++ src/main/java/ApplicationModel/User.java
@@ -0,0 +1,21 @@
+package ApplicationModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    //Attributes
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private List<User> friendList;
+    private String profilePicture;
+
+    //constructor
+    public User(int UserID, String firstName, String lastName, String email, String password, String profilePicture){
+
+
+    }
+}
