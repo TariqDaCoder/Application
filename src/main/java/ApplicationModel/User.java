076ca41ebec10cfd5c272890fc398ee41e8fac2c
kavin <pv3442ia@go.minnstate.edu>
2024-09-22 21:28:19 -0500

    Added main class to scr/main/java

--- src/main/java/ApplicationModel/User.java
+++ src/main/java/ApplicationModel/User.java
@@ -14,8 +14,14 @@ public class User {
     private String profilePicture;
 
     //constructor
-    public User(int UserID, String firstName, String lastName, String email, String password, String profilePicture){
-
-
+    public User(int userID, String firstName, String lastName, String email, String password, String profilePicture){
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.profilePicture = profilePicture;
     }
+
+
 }
